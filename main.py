0e3e882472f830ffaf4e26cb4803323e0ca915c4
yeondon <s25030@gsm.hs.kr>
2026-01-08 14:09:21 +0900

    post 추가

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ items = []
 def root():
     return {"박성호": "시발련"}
 
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return {
+        "message": "아이템 추가 완료",
+        "items": items
+    }
+
 
